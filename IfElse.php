54ce3f34bad47af36247a48c7ab5a4ed8514724a
Archita Das <darchita06@gmail.com>
2025-01-17 12:16:22 +0530

    Initial commit

---
+++ IfElse.php
@@ -0,0 +1,10 @@
+<?php
+$num=70;
+if($num>10 && $num<20){
+    echo "nmumber lies between 10 and 20\n";
+}
+else if($num>20 && $num<60){
+    echo "number lies between 20 and 60\n";
+}
+else echo"number doesnot lie in the range";
+?>
---
+++ intro.php
@@ -0,0 +1,18 @@
+<?php
+$num1=6;
+echo "My first script \n";
+echo "The number is $num1\n";
+echo var_dump($num1);
+echo gettype($num1)."\n";
+$a=10;
+$b=20;
+$sum=($a+$b);
+echo "The sum is : $sum\n";
+$str1="Hello";
+$str2=" World";
+echo $str1.$str2."\n";
+$f1=5.4;
+$f2=4.7;
+$sum1=$f1+$f2;
+echo "The sum is: $sum1\n";
+?>
