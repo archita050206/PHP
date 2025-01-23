8675326bc1df190049b1d981814c2da5a4442998
Archita Das <darchita06@gmail.com>
2025-01-23 13:25:22 +0530

    sum of elements in an array

--- Arr1.php
+++ Arr1.php
@@ -1,7 +1,8 @@
 <?php
 $arr=[1,2,3,4,5,6,7,8,9];
 $sum=0;
-for($i=0;$i<9;$i++){
+$length=count($arr);//to get the length of the array
+for($i=0;$i<$length;$i++){
     $sum+=$arr[$i];
 }
 echo "Sum: $sum";
