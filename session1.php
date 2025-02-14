cabd2119a453df10e656bb70f0a420c1f7235b51
Archita Das <darchita06@gmail.com>
2025-02-14 12:48:42 +0530

    session

--- session1.php
+++ session1.php
@@ -2,5 +2,11 @@
 session_start();
 $_SESSION['name']='Archita';
 echo $_SESSION['name']."\n";
-echo "session is started";
+echo "session is started"."\n";
+$id=session_id();
+echo "Session id is: $id";
+//session_unset(); // value is removed after code so when we try to access it from session2, the variables are not available
+//session_destroy();
+$_SESSION['colours']=["red","green","blue","purple","orange"];
+
 ?>
--- session2.php
+++ session2.php
@@ -1,5 +1,12 @@
 <?php
 session_start();
+if(session_status()===PHP_SESSION_NONE){// to check the status of the session
+    session_start();
+    echo "Session is started";
+}
+else{
+    echo "Session is already started";
+}
 if(isset($_SESSION['name'])){
     echo "Welcome ". $_SESSION['name'];
 
@@ -7,5 +14,12 @@ if(isset($_SESSION['name'])){
 else{
     echo 'No session found';
 }
+if(isset($_SESSION['colours'])){
+    echo "Colours available"."\n";
+    
+    foreach ($_SESSION['colours'] as $colour) {
+        echo $colour . " ";
+    }
+    }
 
 ?>
