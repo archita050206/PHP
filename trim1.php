6836823cf7208af4f3f6f9f3dd973c4708e1cdc0
Archita Das <darchita06@gmail.com>
2025-02-22 01:00:04 +0530

    trim function

--- trim1.php
+++ trim1.php
@@ -7,7 +7,7 @@ $str1="  hello   ";
 echo (trim($str1))."\n";
 //implode 
 //explode
-
+// using fgets to take user input: ttakes user input from the entire line
 $str2="Hello world----";
 echo(str_replace("-","",$str2));
 ?>
