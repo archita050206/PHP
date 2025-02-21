e229ff364f02e840bb6c32cc9572183cbbf67bff
Archita Das <darchita06@gmail.com>
2025-02-21 11:37:10 +0530

    trim

--- trim1.php
+++ trim1.php
@@ -4,7 +4,10 @@ echo $str."\n";
 echo (trim($str));
 echo "\n";
 $str1="  hello   ";
-echo (trim($str1));
+echo (trim($str1))."\n";
 //implode 
 //explode
+
+$str2="Hello world----";
+echo(str_replace("-","",$str2));
 ?>
