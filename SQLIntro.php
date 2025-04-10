b768b62e775101ecb0935b0187ec5fb98334ce8b
Archita Das <darchita06@gmail.com>
2025-04-10 19:26:49 +0530

    commit

--- SQLIntro.php
+++ SQLIntro.php
@@ -1,5 +1,14 @@
 <?php
 // to view all data
 //where data: where city=" ", where population>'1234567', where country='abc', where country='india' and population > 100000
+//order by ans/desc
+//try inserting data from code, 10 rows with the help of loop
+// create one more table with similar structure with same column names
+//merge the 2 tables by union
 
+// develop an employeee management sys using mysql that enables students to perform essential database operations on employee related data:
+//     truncate to clear all records from <table>
+//     where to filter specific rows based on conditions 
+//     order by to sort data based on one or more columns
+//     group by to perform aggregation and summarise data
 ?>
